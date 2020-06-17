a6a822ba35ce17a01ddc859cb199d10afec73fec
chaos2171053 <chaos2171053@gmail.com>
2020-06-17 15:05:46 +0800

    feat(user): add admin

--- database/seeds/WeiboUserSeeder.php
+++ database/seeds/WeiboUserSeeder.php
@@ -8,5 +8,10 @@ class WeiboUserSeeder extends Seeder
     public function run()
     {
         $users = factory(User::class, 50)->create();
+        $user = User::find(1);
+        $user->name = 'chaos';
+        $user->email = 'chaos@gmail.com';
+        $user->is_admin = true;
+        $user->save();
     }
 }
